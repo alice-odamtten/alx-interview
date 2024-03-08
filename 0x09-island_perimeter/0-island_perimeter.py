13383efb5b44b5b8e11daf39a22b97446726b54b
alice-odamtten <sakyibeaodamtten>
2024-03-08 01:16:29 +0000

    test 1

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Island perimeter computing module.
-"""
+'''Accessing and iterating over elements in a 2D array.'''
 
 
 def island_perimeter(grid):
-    """Computes the perimeter of an island with no lakes.
-    """
+    ''' returns the perimeter of the island described '''
     perimeter = 0
     if type(grid) != list:
         return 0
@@ -22,26 +20,4 @@ def island_perimeter(grid):
                 j == 0 or row[j - 1] == 0,
             )
             perimeter += sum(edges)
-    return perimeter
-
-
-
-#!/usr/bin/python3
-'''Accessing and iterating over elements in a 2D array.'''
-
-
-def island_perimeter(grid):
-    ''' returns the perimeter of the island described '''
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                    if j > 0 and grid[i][j - 1] == 1:
-                        perimeter -= 2
-
     return perimeter
